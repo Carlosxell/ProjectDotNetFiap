8f31afb9315615ac36d9169dde8a49adcb31d36e
Carlos Augusto <carlos.xell.3@gmail.com>
2020-04-10 13:20:15 -0300

    tentativa de conexão com o banco de dados

--- Repository/DataBaseContext.cs
+++ Repository/DataBaseContext.cs
@@ -3,46 +3,37 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Data.Entity;
+// using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using Microsoft.Extensions.Configuration;
-using Oracle.EntityFrameworkCore;
+// using Oracle.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using DadosSensoresMvc.Models;
 
 namespace DadosSensoresMvc.Repository
 {
-	public class DataBaseContext: Microsoft.EntityFrameworkCore.DbContext
+	public class DataBaseContext: DbContext
 	{
-		// public Context() : base("name=OracleConnectionString")
-		// {
-		// }
+		public AppContext(DbContextOptions options): base(options)
+		{}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
+			var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
 			if (!optionsBuilder.IsConfigured)
 			{
-				var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
 				optionsBuilder.UseOracle(config.GetConnectionString("OracleConnection"));
 			}
 		}
 
-		/*  Quando utilizando o Entity com a definição 'System.Data.Entity.DbContext',
-		 *  o mesmo não permite configurar o método 'OnConfiguring', quando utilizado como está aqui,
-		 *  apontando para 'Microsoft.EntityFrameworkCore.DbContext', o mesmo não permite configurar
-		 *  o método abaixo, devido a isso preferi a configuração ao invés da string acima
-		   
-		protected override void OnModelCreating(DbModelBuilder modelBuilder)
-		{
+		public DbSet<DadosSensores> DadosSensores { get; set; }
 
-			modelBuilder.HasDefaultSchema("PF0954");
-
-			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-		}
-		*/
-
-		public System.Data.Entity.DbSet<DadosSensores> DadosSensores { get; set; }
-
-		public System.Data.Entity.DbSet<Contato> Contato { get; set; }
+		public DbSet<Contato> Contato { get; set; }
 	}
 }
